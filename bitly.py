05787db7e52bbda8f9d5147d074cd9b1cd1e2edb
Ilya <Dragoilya@yandex.ru>
2022-10-10 17:46:19 +0300

    Changed names v2

--- bitly.py
+++ bitly.py
@@ -2,33 +2,36 @@ from urllib.parse import urlparse
 from dotenv import dotenv_values
 import requests
 
-DOMAIN = "api-ssl.bitly.com/"
-PROTOCOL = "https://"
-
 
 def shorten_bitlink(url, token):
-    path = "v4/shorten"
+    api_endpoint = f"https://api-ssl.bitly.com/v4/shorten"
     headers = {"Authorization": f"Bearer {token}"}
     body = {"long_url": url}
-    response = requests.post(f"{PROTOCOL}{DOMAIN}{path}", headers=headers, json=body)
+    response = requests.post(api_endpoint, headers=headers, json=body)
     response.raise_for_status()
     return response.json()["id"]
 
 
 def count_clicks(url, token):
     parsed_url = urlparse(url)
-    path = f"v4/bitlinks/{parsed_url.netloc}{parsed_url.path}/clicks/summary"
+    api_endpoint = (
+        f"https://api-ssl.bitly.com/v4/bitlinks/"
+        f"{parsed_url.netloc}{parsed_url.path}/clicks/summary"
+    )
     headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(f"{PROTOCOL}{DOMAIN}{path}", headers=headers)
+    response = requests.get(api_endpoint, headers=headers)
     response.raise_for_status()
     return response.json()["total_clicks"]
 
 
 def is_bitlink(url, token):
     parsed_url = urlparse(url)
-    path = f"v4/bitlinks/{parsed_url.netloc}{parsed_url.path}"
+    api_endpoint = (
+        f"https://api-ssl.bitly.com/v4/bitlinks/"
+        f"{parsed_url.netloc}{parsed_url.path}"
+    )
     headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(f"{PROTOCOL}{DOMAIN}{path}", headers=headers)
+    response = requests.get(api_endpoint, headers=headers)
     return response.ok
 
 
